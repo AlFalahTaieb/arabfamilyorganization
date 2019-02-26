c14bdaa691c829ff987def4b8c66f42da47823f4
AlFalah <taiebalfalah@gmail.com>
2019-02-26 16:38:00 +0100

    add responsive

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -2,23 +2,50 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import './Navbar.css'
+import logo from '../../images/arabfamily.jpg'
+
+
 
 
 const Navbar = (props) => {
 
 
     return (
-        <nav className="container">
-            {/* <ul className="container"> */}
-            <ul class="nav-links">
-                <Link to='/' className=".nav-item ">
-                    الصفحة الرئيسية
-</Link>
-                <Link to='/' className=".nav-item ">حول المنظمة</Link>
-                < Link to='/' className=".nav-item">منظمة الأسرة العربية</Link>
-            </ul>
+        <div className="header">
 
-        </nav>
+            <a href="#" className='sidenav-trigger hide-on-med-and-up' data-target='mobile-nav'>
+                <i class="material-icons">menu</i>
+            </a>
+            <nav className="container" >
+                <h6 className='hide-on-med-and-up'>
+                    منظمة الأسرة العربية
+            </h6>
+                <ul className="nav-links hide-on-small-only" >
+
+                    <Link to='/' className="nav-item">الصفحة الرئيسية</Link>
+                    <Link to='/' className="nav-item">حول المنظمة</Link>
+                    <Link to='/' className="nav-item">منظمة الأسرة العربية</Link>
+                    <Link to='/' className="nav-item">الفعاليات</Link>
+                    <Link to='/' className="nav-item">العضوية</Link>
+                    <Link to='/' className="nav-item">مجلس سفراء الأسرة العربية</Link>
+
+                </ul>
+
+            </nav>
+
+            <ul className="sidenav" id='mobile-nav'>
+                <li to='/' className="nav-item">الصفحة الرئيسية</li>
+                <li to='/' className="nav-item">حول المنظمة</li>
+                <li to='/' className="nav-item">منظمة الأسرة العربية</li>
+                <li to='/' className="nav-item">الفعاليات</li>
+                <li to='/' className="nav-item">العضوية</li>
+                <li to='/' className="nav-item">مجلس سفراء الأسرة العربية</li>
+
+            </ul>
+            <a class="logo hide-on-med-and-down">
+                <img src={logo} className='logo' />
+            </a>
+        </div>
     )
 }
 
