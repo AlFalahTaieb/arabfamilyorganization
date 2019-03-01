145d7061f86a3ab3e5c69edbb448883493e663db
AlFalah <taiebalfalah@gmail.com>
2019-03-01 23:52:11 +0100

    Still trying to fix Carousel

--- src/components/Pages/PageTest/PageTest.js
+++ src/components/Pages/PageTest/PageTest.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import Siema from 'siema'
-// import './Page1.css'
+import './PageTest.css'
 import M from "materialize-css/dist/js/materialize.min.js";
 
 class PageTest extends Component {
@@ -15,152 +15,121 @@ class PageTest extends Component {
         super(props)
         this.state = {
 
-            // family1: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family1.jpg',
-            // family2: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family2.jpg',
-            // family3: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family3.jpg',
-            // family4: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family4.jpg',
-            // family5: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family5.jpg',
-            // family6: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family6.jpg',
-            // family7: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family7.jpg',
-            // family8: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family8.jpg',
-            // family9: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family9.jpg',
-            // family10: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family10.jpg',
-            // family11: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family11.jpg',
-            // family12: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family12.jpg',
+            family1: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family1.jpg',
+            family2: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family2.jpg',
+            family3: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family3.jpg',
+            family4: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family4.jpg',
+            family5: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family5.jpg',
+            family6: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family6.jpg',
+            family7: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family7.jpg',
+            family8: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family8.jpg',
+            family9: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family9.jpg',
+            family10: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family10.jpg',
+            family11: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family11.jpg',
+            family12: 'https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family12.jpg',
             data: []
         }
 
 
     }
-    // setInterval(() => {
-    //     carousel.next()
-    // }, 3000)
+
     componentWillMount() {
-        // const carousel = new Siema({
-        //     selector: '.taieb',
-        //     easing: 'cubic-bezier(.17,.67,.32,1.34)',
-        //     perPage: 1,
-        //     duration: 1500,
-        //     startIndex: 0,
-        //     draggable: true,
-        //     multipleDrag: true,
-        //     threshold: 20,
-        //     loop: true,
-        //     rtl: true,
-        // })
-        // setInterval(() => {
-        //     carousel.next()
-        // }, 5000)
-        // var elems = document.querySelectorAll('.carousel-slider')
-        // M.Carousel.init({
-        //      fullWidth: true,
-        //    });
+
     }
-     async componentDidMount() {
-        var elems = document.querySelectorAll('.carousel');
-        var instances = M.Carousel.init(elems);
-        var instance = M.Carousel.getInstance(elems);
-        //  setInterval(() => {
-        //     instance('next')
-        // }, 5000)
+    async componentDidMount() {
 
-        let response = await fetch('https://res.cloudinary.com/drvdkcdnl/image/upload/v1551265411/family1.jpg')
-        let data = await response.url
+        let elems = document.querySelectorAll('.carousel');
+        let instances = M.Carousel.init(elems, {
+            fullWidth: true,
+            indicators: true
+        });
+        // let instance = M.Carousel.getInstance(elems);
+        // instances('next')
+        // instance('next')
+        // setInterval(function () { instance('next') }, 5000)
+    setInterval(() => {
+            var instance = M.Carousel.getInstance(elems);
+            instance.next();
+        }, 5000)
 
-        // _isMounted = true
-
-        console.log(data)
-        return this.setState({
-            family1: data
-        })
-      
     }
 
     render() {
-        let family1 = this.state.family1
-        let text1 = 'CECI EST UN TEST PAGE'
+        let heading = 'صور فعالية عرس زايد العربي'
+        let hadf2 = `رعاية الأسرة العربية والنهوض بها وتمكينها من القيام بوظائفها الاجتماعية والاقتصادية والتربوية والثقافيةشهد معالي الشيخ سعيد بن طحنون آل نهيان «عرس زايد العربي الجماعي الأول»، والذي نظمته «منظمة الأسرة العربية»، مساء أمس -السبت- في مركز «إكسبو الشارقة»، بمشاركة 500 عريس وعروس، من أبناء الدول العربية المقيمين على أرض دولة الإمارات العربية المتحدة، والذي يأتي تزامناً مع «عام زايد 2018»، ومئوية الأب المؤسس.`
+        let hadf3 = `كما حضر الحفل الشيخ محمد بن حميد القاسمي، مدير دائرة الإحصاء والتنمية المجتمعية بالشارقة، والدكتور ماجد سعيد النعيمي، رئيس الديوان الأميري بعجمان، وسيف بن محمد المدفع، الرئيس التنفيذي لمركز «إكسبو الشارقة»، ورجل الأعمال عبد الرحيم الزرعوني، وجمال عبيد البح رئيس منظمة الأسرة العربية، وعدد من رجال السلك الدبلوماسي والشخصيات العربية، والفنانين. `
+        let hadf4 = `وشارك في العرس عرسان يمثلون عدداً من البلدان العربية من: مصر، الأردن، فلسطين، اليمن، السودان، جزر القمر، الجزائر، المغرب، ليبيا، العراق، سلطنة عمان، سوريا، بالإضافة لعدد من العرسان من باكستان، وإيران وثلاثة من المواطنين من الإمارات العربية المتحدة. وأكد جمال البح، في كلمة بالعرس، أن «عرس زايد العربي الأول» يمثل علامة مضيئة في سجلات الترابط الأسري، ويؤكد على السلام والتآخي بين الشعوب العربية والصديق`
         return (
 
 
             <div>
-                <div className="carousel carousel-slider">
-                    <a className="carousel-item" href="#one!"><img src={family1} /></a>
-                    <a className="carousel-item" href="#two!"><img src="https://lorempixel.com/800/400/food/2" /></a>
-                    <a className="carousel-item" href="#three!"><img src="https://lorempixel.com/800/400/food/3" /></a>
-                    <a className="carousel-item" href="#four!"><img src="https://lorempixel.com/800/400/food/4" /></a>
-                </div>
-                <h1>wwaa</h1>
-
-
                 <div class="row">
-                    {/* <section className="container section" > */}
-
-                    {/* <div  class='carousel carousel-slider data-indicator'> */}
-                        <a href="#one!" className='carousel-item'>
-                            <img src={family1} alt='' />
-                        </a>
-                        <a href="#one!" className='carousel-item'>
-                            <img src={this.state.family2} alt='' />
-                        </a>
-                        {/* <a href="#two!" className='carousel-item'>
-                                <img src={this.state.family2} alt='' />
+                    <h1>{heading} </h1>
+                    <section className="container section" >
+                        <div class="carousel carousel-slider" data-indicators='true'>
+                            <a href="#one!" className='carousel-item'>
+                                <img src={this.state.family1} alt='' />
+                            </a>
+                            <a href="#two!" className='carousel-item'>
+                                <img class='fixed-height' src={this.state.family2} alt='' />
 
                             </a>
                             <a href="#three!" className='carousel-item'>
 
-                                <img src={this.state.family3} alt='' />
+                                <img class='fixed-height' src={this.state.family3} alt='' />
                             </a>
                             <a href="#four!" className='carousel-item'>
 
-                                <img src={this.state.family4} alt='' />
+                                <img class='fixed-height' src={this.state.family4} alt='' />
                             </a>
                             <a href="#five!" className='carousel-item'>
 
-                                <img src={this.state.family5} alt='' />
+                                <img class='fixed-height' src={this.state.family5} alt='' />
                             </a>
                             <a href="#six!" className='carousel-item'>
 
-                                <img src={this.state.family6} alt='' />
+                                <img class='fixed-height' src={this.state.family6} alt='' />
                             </a>
                             <a href="#seven!" className='carousel-item'>
 
-                                <img src={this.state.family7} alt='' />
+                                <img class='fixed-height' src={this.state.family7} alt='' />
                             </a>
                             <a href="#eight!" className='carousel-item'>
 
-                                <img src={this.state.family8} alt='' />
+                                <img class='fixed-height' src={this.state.family8} alt='' />
                             </a>
                             <a href="#nine!" className='carousel-item'>
 
-                                <img src={this.state.family9} alt='' />
+                                <img class='fixed-height' src={this.state.family9} alt='' />
                             </a>
                             <a href="#ten!" className='carousel-item'>
 
-                                <img src={this.state.family10} alt='' />
+                                <img class='fixed-height' src={this.state.family10} alt='' />
                             </a>
                             <a href="#twelve!" className='carousel-item'>
 
-                                <img src={this.state.family11} alt='' />
+                                <img class='fixed-height' src={this.state.family11} alt='' />
                             </a>
                             <a href="#eleven!" className='carousel-item'>
 
-                                <img src={this.state.family12} alt='' />
-                            </a> */}
+                                <img class='fixed-height' src={this.state.family12} alt='' />
+                            </a>
 
 
-                    </div>
+                        </div>
 
-                    <div class="container section" className='s12 m4 l8'>
-                        <p>{text1}</p>
+                        <div class="container section" className='s12 m4 l8'>
+                            <p>{hadf2}</p>
+                            <br />
+                            <p>{hadf3}</p>
+                            <br />
+                            <p>{hadf4}</p>
+                            <br />
 
-                        <br />
+                        </div>
+                    </section>
 
-                    </div>
-
-
-
-
-                {/* </div> */}
+                </div>
 
             </div>
 
